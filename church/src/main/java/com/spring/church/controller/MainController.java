e000424264ee83faf191786e37beda2e1391f259
jcg07 <jcg07@DESKTOP-SMB03E8>
2022-02-14 21:11:27 +0900

    create project

---
+++ church/src/main/java/com/spring/church/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.spring.church.controller;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String main() {
+		String url = "/common/main";
+		return url;
+	}
+}
